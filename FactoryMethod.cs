dbb1bd168e561159eac044d48e7456484db6e19d
Cppshizoid <alex.krivoschecov@gmail.com>
2022-11-12 09:56:56 -0800

    Create FactoryMethod.cs

---
+++ FactoryMethod.cs
@@ -0,0 +1,65 @@
+class Program   //Factory Method
+{
+    static void Main(string[] args)
+    {
+        Developer dev = new PanelDeveloper("OOO ComapanyABOBA");
+        House house2 = dev.Create();
+
+        dev = new WoodDeveloper("PrivateDeveloper");
+        House house = dev.Create();
+
+        Console.ReadLine();
+    }
+}
+// abstract class build company
+abstract class Developer
+{
+    public string Name { get; set; }
+
+    public Developer(string n)
+    {
+        Name = n;
+    }
+    // Factory Method
+    abstract public House Create();
+}
+// build panel houses
+class PanelDeveloper : Developer
+{
+    public PanelDeveloper(string n) : base(n)
+    { }
+
+    public override House Create()
+    {
+        return new PanelHouse();
+    }
+}
+// build wooden houses
+class WoodDeveloper : Developer
+{
+    public WoodDeveloper(string n) : base(n)
+    { }
+
+    public override House Create()
+    {
+        return new WoodHouse();
+    }
+}
+
+abstract class House
+{ }
+
+class PanelHouse : House
+{
+    public PanelHouse()
+    {
+        Console.WriteLine("Panel house is builded");
+    }
+}
+class WoodHouse : House
+{
+    public WoodHouse()
+    {
+        Console.WriteLine("Wooden house is builded");
+    }
+}
