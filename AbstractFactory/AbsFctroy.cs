3b8714a2ec9200a562e7eb6ea5ae131d831c057f
Cppshizoid <alex.krivoschecov@gmail.com>
2022-11-12 13:23:36 -0800

    Create AbsFctroy.cs

---
+++ AbstractFactory/AbsFctroy.cs
@@ -0,0 +1,109 @@
+class AbsFactory
+{
+    static void Main(string[] args)
+    {
+        Hero elf = new Hero(new ElfFactory());
+        elf.Hit();
+        elf.Run();
+
+        Hero voin = new Hero(new VoinFactory());
+        voin.Hit();
+        voin.Run();
+
+        Console.ReadLine();
+    }
+}
+//абстрактный класс - оружие
+abstract class Weapon
+{
+    public abstract void Hit();
+}
+// абстрактный класс движение
+abstract class Movement
+{
+    public abstract void Move();
+}
+
+// класс арбалет
+class Arbalet : Weapon
+{
+    public override void Hit()
+    {
+        Console.WriteLine("Shoot from the crossbow");
+    }
+}
+// класс меч
+class Sword : Weapon
+{
+    public override void Hit()
+    {
+        Console.WriteLine("Hittng sword ");
+    }
+}
+// движение полета
+class FlyMovement : Movement
+{
+    public override void Move()
+    {
+        Console.WriteLine("To fly");
+    }
+}
+// движение - бег
+class RunMovement : Movement
+{
+    public override void Move()
+    {
+        Console.WriteLine("To run");
+    }
+}
+// класс абстрактной фабрики
+abstract class HeroFactory
+{
+    public abstract Movement CreateMovement();
+    public abstract Weapon CreateWeapon();
+}
+// Фабрика создания летящего героя с арбалетом
+class ElfFactory : HeroFactory
+{
+    public override Movement CreateMovement()
+    {
+        return new FlyMovement();
+    }
+
+    public override Weapon CreateWeapon()
+    {
+        return new Arbalet();
+    }
+}
+// Фабрика создания бегущего героя с мечом
+class VoinFactory : HeroFactory
+{
+    public override Movement CreateMovement()
+    {
+        return new RunMovement();
+    }
+
+    public override Weapon CreateWeapon()
+    {
+        return new Sword();
+    }
+}
+// клиент - сам супергерой
+class Hero
+{
+    private Weapon weapon;
+    private Movement movement;
+    public Hero(HeroFactory factory)
+    {
+        weapon = factory.CreateWeapon();
+        movement = factory.CreateMovement();
+    }
+    public void Run()
+    {
+        movement.Move();
+    }
+    public void Hit()
+    {
+        weapon.Hit();
+    }
+}
